3b4292a0ba93767be599a20d2660d5eb5ee26241
Lucas Chen <chen@spectralmd.com>
2022-12-05 13:58:24 -0600

    First commit

---
+++ ImagingApp/ImagingAppWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ImagingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static NamedPipeClientStream PipeClientStream { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += Window_Loaded;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (mePlayer.Source != null)
+            {
+                if (mePlayer.NaturalDuration.HasTimeSpan)
+                    lblStatus.Content = String.Format("{0} / {1}", mePlayer.Position.ToString(@"mm\:ss"), mePlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+            }
+            else
+                lblStatus.Content = "No file selected...";
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Stop();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //using NamedPipeClientStream namedPipeClientStream = new("pipeStream1");
+            //namedPipeClientStream.Connect();
+            //namedPipeClientStream.ReadMode = PipeTransmissionMode.Message;
+
+            //Task.Factory.StartNew(() =>
+            //{
+            //    while (true)
+            //    {
+            //        string message = ReadMessage(PipeClientStream);
+            //        switch (message)
+            //        {
+            //            case "minimize":
+            //                Debug.WriteLine("The ImageApp receive the minimize command.");
+            //                App.Current.MainWindow.WindowState = WindowState.Minimized;
+            //                break;
+            //            case "maximize":
+            //                Debug.WriteLine("The ImageApp receive the minimize command.");
+            //                App.Current.MainWindow.WindowState = WindowState.Maximized;
+            //                break;
+            //        }
+            //    }
+            //});
+        }
+
+        #region Read Pipe Message
+
+        public static string ReadMessage(PipeStream s)
+        {
+            MemoryStream ms = new();
+            byte[] buffer = new byte[0x1000];      // Read in 4k byte blocks 
+
+            do
+            {
+                ms.Write(buffer, 0, s.Read(buffer, 0, buffer.Length));
+            }
+            while (!s.IsMessageComplete);
+
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+
+        #endregion
+    }
+}
---
+++ MainApp/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static NamedPipeServerStream NamedPipeServerStream { get; private set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += Window_Loaded;
+        }
+
+        private void  Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                NamedPipeServerStream = new("pipeStream1", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+                NamedPipeServerStream.WaitForConnection();
+            });
+        }
+
+        private void Invoke_Imaging_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"D:\Test\Invoke.Test.Projects\ImagingApp\bin\Debug\net6.0-windows\ImagingApp.exe";
+            string[] arguments = Array.Empty<string>();
+
+            // start
+            ProcessStartInfo psi = new()
+            {
+                FileName = path,
+                CreateNoWindow = false,
+                WindowStyle = ProcessWindowStyle.Minimized,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+
+            foreach (var argument in arguments)
+            {
+                argument.Trim();
+                psi.ArgumentList.Add(argument);
+            }
+
+            Process imagingApp = new();
+            imagingApp.StartInfo = psi;
+            imagingApp.OutputDataReceived += (sender, e) => Console.WriteLine("Imaging app: " + e.Data);
+            imagingApp.ErrorDataReceived += (sender, e) => Console.WriteLine("Error in imaging app: " + e.Data);
+            imagingApp.Start();
+            imagingApp.BeginOutputReadLine();
+            imagingApp.BeginErrorReadLine();
+            imagingApp.WaitForExit();
+        }
+
+        private void Min_Imaging_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("The MainApp send minimize command.");
+            if (NamedPipeServerStream.IsConnected)
+            {
+                Debug.WriteLine("The MainApp connect with ImagingApp, and send commands.");
+                byte[] msg = Encoding.UTF8.GetBytes("minimize");
+                NamedPipeServerStream.WriteAsync(msg, 0, msg.Length);
+            }
+        }
+
+        private void Max_Imagin_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("The MainApp send maximize command.");
+            if (NamedPipeServerStream.IsConnected)
+            {
+                Debug.WriteLine("The MainApp connect with ImagingApp, and send commands.");
+                byte[] msg = Encoding.UTF8.GetBytes("maximize");
+                NamedPipeServerStream.WriteAsync(msg, 0, msg.Length);
+            }
+        }
+    }
+}
